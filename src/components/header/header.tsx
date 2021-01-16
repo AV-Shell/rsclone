8a36fa612189bd10b3aca752dd4694abc708bb5f
Evgeniy <e.ortsukh@gmail.com>
2021-01-16 17:50:11 +0300

    header/feat: add login menu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import './header.scss';
 import BurgerMenu from '../burger-menu';
-function Header() {
+import { Link } from "react-router-dom";
+import { headerProps } from '../../constants/interfaces';
 
- 
-   return (
+function Header(props: headerProps) {
+  console.log('header props:', props);
+  const {
+    isDarkTheme, isAuthorizated
+  } = props;
+  const loginMenu = <div className='header-login-menu'>
+    <Link to='/login ' className='header-login'> Login</Link>
+    <Link to='/registration' className='header-login'> Login</Link>
+  </div>;
+  const styleTheme = isDarkTheme ? 'Dark theme' : 'Light theme';
+  let headerSwitchMenu = isAuthorizated ? <BurgerMenu /> : loginMenu;
+  console.log(headerSwitchMenu);
+
+  return (
     <header className="header">
-      <BurgerMenu/>
-         <h2>Header component</h2>
-         <div className="header-theme">
-         <div className="base-text-label">Light theme</div>
-        {/* <Base.TextLabel value={setThemeTextLabel()} /> */}
-        <div className="header-toggle click" >
+      {headerSwitchMenu}
+      <h2>Header component</h2>
+      <div className="header-theme">
+        <div className="base-text-label">{styleTheme}</div>
+        <div className="header-toggle click" onClick={props.toggleTheme}>
           <span></span>
         </div>
       </div>
-        
     </header>
   );
- 
-
 }
 
 export default Header;
