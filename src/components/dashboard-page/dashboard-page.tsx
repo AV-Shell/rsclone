e05aeda82d778954a743a74cc95ff1e765bf4f86
Evgeniy <e.ortsukh@gmail.com>
2021-01-24 22:34:00 +0300

    dasboard/feat:hot fix

--- src/components/dashboard-page/dashboard-page.tsx
+++ src/components/dashboard-page/dashboard-page.tsx
@@ -1,49 +1,51 @@
 import React, {
-  useRef, useEffect, useState, ReactNode, useLayoutEffect
-} from 'react';
+  useRef,
+  useEffect,
+  useState,
+  ReactNode,
+  useLayoutEffect,
+} from "react";
 import Chart from "chart.js";
-import './dashboard-page.scss';
-import { dashboardProps } from '../../constants/interfaces';
-import userImg from '../header/assets/1.png'
-import { AVA_URL } from '../../constants/constants'
-
+import "./dashboard-page.scss";
+import { dashboardProps } from "../../constants/interfaces";
+import userImg from "../header/assets/1.png";
+import { AVA_URL } from "../../constants/constants";
 
 function DashboardPage(props: dashboardProps) {
-  const {
-    isDarkTheme
-  } = props;
-  const chartoption = {
+  const { isDarkTheme } = props;
+  const chartOption = {
     legend: {
       display: false,
-
     },
     scales: {
       yAxes: [
         {
           ticks: {
             stepSize: 10,
-            suggestedMax: 100
+            suggestedMax: 100,
           },
         },
       ],
     },
-  }
-  const [optionMenuItems, setOptionMenuItems] = useState(chartoption);
+  };
+  const [optionMenuItems, setOptionMenuItems] = useState(chartOption);
   console.log(props);
-  const avatarUrl = `${AVA_URL}` + 'ava_18.png'
+  const avatarUrl = `${AVA_URL}` + "ava_18.png";
 
   useLayoutEffect(() => {
-    const ctx: any = document.getElementById('myChart');
+    const ctx: any = document.getElementById("myChart");
     new Chart(ctx, {
       type: "line",
       data: {
         labels: ["0s", "100s", "30s", "40s", "50s", "60s", "60s", "60s", "60s"],
-        datasets: [{
-          // label: "words progress",
-          data: [0, 5, 15, 20, 32, 40],
-        }]
+        datasets: [
+          {
+            // label: "words progress",
+            data: [0, 5, 15, 20, 32, 40],
+          },
+        ],
       },
-      options: optionMenuItems
+      options: optionMenuItems,
     });
     CreateLineChart("CardChart");
     CreatePieChart();
@@ -55,12 +57,14 @@ function DashboardPage(props: dashboardProps) {
       type: "line",
       data: {
         labels: ["0s", "100s", "30s", "40s", "50s", "60s", "60s", "60s", "60s"],
-        datasets: [{
-          // label: "words progress",
-          data: [0, 5, 15, 20, 32, 40],
-        }]
+        datasets: [
+          {
+            // label: "words progress",
+            data: [0, 5, 15, 20, 32, 40],
+          },
+        ],
       },
-      options: optionMenuItems
+      options: optionMenuItems,
     });
   }
   function CreatePieChart() {
@@ -69,101 +73,109 @@ function DashboardPage(props: dashboardProps) {
     let lastend = 0;
     const data = [60, 210];
     let myTotal = 0;
-    const myColor = ['red', '#95b524'];
-    const labels = ['35%', '65%'];
-
+    const myColor = ["red", "#95b524"];
+    const labels = ["35%", "65%"];
+    const off = 10;
+    const w = (canvas.width - off) / 2;
+    const h = (canvas.height - off) / 2;
     for (let i = 0; i < data.length; i++) {
       myTotal += data[i];
     }
 
     // make the chart 10 px smaller to fit on canvas
-    const off = 10
-    const w = (canvas.width - off) / 2
-    const h = (canvas.height - off) / 2
+
     for (let i = 0; i < data.length; i++) {
       ctx.fillStyle = myColor[i];
-      ctx.strokeStyle = 'white';
+      ctx.strokeStyle = "white";
       ctx.lineWidth = 2;
       ctx.beginPath();
       ctx.moveTo(w, h);
-      const len = (data[i] / myTotal) * 2 * Math.PI
-      const r = h - off / 2
+      const len = (data[i] / myTotal) * 2 * Math.PI;
+      const r = h - off / 2;
       ctx.arc(w, h, r, lastend, lastend + len, false);
       ctx.lineTo(w, h);
       ctx.fill();
       ctx.stroke();
-      ctx.fillStyle = 'white';
+      ctx.fillStyle = "white";
       ctx.font = "20px Arial";
       ctx.textAlign = "center";
       ctx.textBaseline = "middle";
-      const mid = lastend + len / 2
-      ctx.fillText(labels[i], w + Math.cos(mid) * (r / 2), h + Math.sin(mid) * (r / 2));
+      const mid = lastend + len / 2;
+      ctx.fillText(
+        labels[i],
+        w + Math.cos(mid) * (r / 2),
+        h + Math.sin(mid) * (r / 2)
+      );
       lastend += Math.PI * 2 * (data[i] / myTotal);
     }
   }
   // Component code start
   return (
     <div className="dashboard-page">
-      <div className='dashboard-page-first_column'>
-        <div className='dashboard-page-first_column-user_info'>
-          <div className='dashboard-page-first_column-user_info-img'><img src={avatarUrl} alt="avatar" /></div>
-          <div className='dashboard-page-first_column-user_info-data'>
+      <div className="dashboard-page-first_column">
+        <div className="dashboard-page-first_column-user_info">
+          <div className="dashboard-page-first_column-user_info-img">
+            <img src={avatarUrl} alt="avatar" />
+          </div>
+          <div className="dashboard-page-first_column-user_info-data">
             <h3>John Doe</h3>
             <p>Newbie</p>
             <p>Learning from 20.01.2021</p>
           </div>
         </div>
-        <div className='dashboard-page-first_column-chart_progress'>
+        <div className="dashboard-page-first_column-chart_progress">
           <canvas id="myChart" width="560" height="310"></canvas>
           <div>All words progress</div>
         </div>
       </div>
-      <div className='dashboard-page-second_column'>
-        <div className='dashboard-page-second_column-info'>
-          <div className='dashboard-page-second_column-info-all_words'>
+      <div className="dashboard-page-second_column">
+        <div className="dashboard-page-second_column-info">
+          <div className="dashboard-page-second_column-info-all_words">
             New words:
-             <p className='dashboard-page-second_column-info-card-stat'>3</p>
+            <p className="dashboard-page-second_column-info-card-stat">3</p>
           </div>
-          <div className='dashboard-page-second_column-info-best_series'>Best streak:
-          <p className='dashboard-page-second_column-info-card-stat'>3</p>
+          <div className="dashboard-page-second_column-info-best_series">
+            Best streak:
+            <p className="dashboard-page-second_column-info-card-stat">3</p>
           </div>
-          <div className='dashboard-page-second_column-info-all_cards'>
+          <div className="dashboard-page-second_column-info-all_cards">
             Total passed cards:
-            <p className='dashboard-page-second_column-info-card-stat'>3</p>
+            <p className="dashboard-page-second_column-info-card-stat">3</p>
           </div>
-          <div className='dashboard-page-second_column-info-card_point'> Your point:
-          <p className='dashboard-page-second_column-info-card-stat'>3</p>
+          <div className="dashboard-page-second_column-info-card_point">
+           
+            Your point:
+            <p className="dashboard-page-second_column-info-card-stat">3</p>
           </div>
-
         </div>
-        <div className='dashboard-page-second_column-chart_diaram'>
-          <div className='piechart'>
-            <div className='piechart-legend'>
-              <div className='piechart-legend-correct'></div>
+        <div className="dashboard-page-second_column-chart_diagram">
+          <div className="piechart">
+            <div className="piechart-legend">
+              <div className="piechart-legend-correct"></div>
               <span>- correct answer</span>
             </div>
-            <div className='piechart-legend'>
-              <div className='piechart-legend-incorrect'>  </div>
+            <div className="piechart-legend">
+              <div className="piechart-legend-incorrect"> </div>
               <span>- incorrect answer</span>
             </div>
           </div>
           <canvas id="piechart" width="500" height="275"></canvas>
-          <div>
-            Ratio of correct and incorrect answers for all time</div>
+          <div>Ratio of correct and incorrect answers for all time</div>
         </div>
       </div>
-      <div className='dashboard-page-third_column'>
-        <div className='dashboard-page-third_column-daily'>
+      <div className="dashboard-page-third_column">
+        <div className="dashboard-page-third_column-daily">
           <span>Daily goal:</span>
           <p>Complete 50 Cards</p>
           <p>
-            Today you have completed 0 cards. Complete 50 cards to reach the goal.</p>
-          <p>Your best streak  today: 0</p>
+            Today you have completed 0 cards. Complete 50 cards to reach the
+            goal.
+          </p>
+          <p>Your best streak today: 0</p>
         </div>
-        <div className='dashboard-page-third_column-chart_card_progress'>
+        <div className="dashboard-page-third_column-chart_card_progress">
           <canvas id="CardChart" width="460" height="290"></canvas>
-          <div>
-            Number of cards made by day</div>
+          <div>Number of cards made by day</div>
         </div>
       </div>
     </div>
@@ -171,9 +183,3 @@ function DashboardPage(props: dashboardProps) {
 }
 
 export default DashboardPage;
-// return (
-//   <div className="dashboard-page">
-//     <h2>Dashboard page</h2>
-//   </div>
-// );
-// }
