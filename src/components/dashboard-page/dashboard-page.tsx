b6db4b14a0402740d443517a9b406cc043e31567
Ортюх Евгений Иосифович <e.ortuh@bc.main.by>
2021-01-28 14:48:54 +0300

    dashboard/feat:fix style add responsive

--- src/components/dashboard-page/dashboard-page.tsx
+++ src/components/dashboard-page/dashboard-page.tsx
@@ -1,70 +1,67 @@
-import React, {
- 
-  useState,
- 
-  useLayoutEffect,
-} from "react";
+import React, { useState, useLayoutEffect, useEffect } from "react";
 import Chart from "chart.js";
 import "./dashboard-page.scss";
 import { dashboardProps } from "../../constants/interfaces";
 import { AVA_URL } from "../../constants/constants";
+import { Line } from 'react-chartjs-2';
+import { RU, EN } from './langs';
+
 
 function DashboardPage(props: dashboardProps) {
-  const { isDarkTheme } = props;
-  const chartOption = {
-    legend: {
-      display: false,
-    },
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            stepSize: 10,
-            suggestedMax: 100,
-          },
+  const {
+    isDarkTheme,
+    isLanguageRU,
+    settings,
+  } = props;
+  const data:any = {
+    labels: [
+      '10/04', '10/05', 
+      '10/06', '10/07', 
+      '10/08', '10/09', 
+      '10/10', '10/11', 
+      '10/12', '10/13', 
+      '10/14', '10/15'
+    ],
+    datasets: [
+      {
+        
+        data: [22,19,27,23,22,24,17,25,23,24,20,19],
+        fill: true,          
+        borderColor: '#7E8299'  
+      }
+    ]
+  }
+  const options:any = {legend:{  display: false,},
+  maintainAspectRatio : false,
+  responsive: true, 
+  scales: {
+    xAxes: [
+      {
+        ticks: {
+          autoskip: true,
+          autoSkipPadding: 30
         },
-      ],
-    },
-  };
-  const [optionMenuItems, setOptionMenuItems] = useState(chartOption);
-  console.log(props);
-  const avatarUrl = `${AVA_URL}ava_18.png`;
-
-  useLayoutEffect(() => {
-    const ctx:any = document.getElementById("myChart");
-    new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: ["0s", "100s", "30s", "40s", "50s", "60s", "60s", "60s", "60s"],
-        datasets: [
-          {
-            // label: "words progress",
-            data: [0, 5, 15, 20, 32, 40],
-          },
-        ],
       },
-      options: optionMenuItems,
-    });
-    CreateLineChart("CardChart");
-    CreatePieChart();
+    ],
+  },
+  
+  }
+
+  
+  console.log(props);
+  const avatarUrl = `${AVA_URL}ava_${settings.optional.avatarID}.png`;
+  const userName = localStorage.getItem('userName')?.slice(1,-1);
+  const cardsPerDay = props.settings.optional.cardsPerDay;
+  const comonProgress = props.settings.optional.commonProgress;
+  let currentLang = isLanguageRU ? RU : EN;
+  useEffect(() => {
+    currentLang = isLanguageRU ? RU : EN;
+  }, [ isLanguageRU ]);
+  useLayoutEffect(() => {
+   CreatePieChart();
   }, []);
 
-  function CreateLineChart(id: string) {
-    const ctx: any = document.getElementById(id);
-    new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: ["0s", "100s", "30s", "40s", "50s", "60s", "60s", "60s", "60s"],
-        datasets: [
-          {
-            // label: "words progress",
-            data: [0, 5, 15, 20, 32, 40],
-          },
-        ],
-      },
-      options: optionMenuItems,
-    });
-  }
+  
   function CreatePieChart() {
     const canvas: any = document.getElementById("piechart");
     const ctx: any = canvas.getContext("2d");
@@ -79,9 +76,6 @@ function DashboardPage(props: dashboardProps) {
     for (let i = 0; i < data.length; i++) {
       myTotal += data[i];
     }
-
-    // make the chart 10 px smaller to fit on canvas
-
     for (let i = 0; i < data.length; i++) {
       ctx.fillStyle = myColor[i];
       ctx.strokeStyle = "white";
@@ -107,75 +101,76 @@ function DashboardPage(props: dashboardProps) {
       lastend += Math.PI * 2 * (data[i] / myTotal);
     }
   }
+  
   // Component code start
   return (
-    <div className="dashboard-page">
-      <div className="dashboard-page-first_column">
-        <div className="dashboard-page-first_column-user_info">
-          <div className="dashboard-page-first_column-user_info-img">
-            <img src={avatarUrl} alt="avatar" />
-          </div>
-          <div className="dashboard-page-first_column-user_info-data">
-            <h3>John Doe</h3>
-            <p>Newbie</p>
-            <p>Learning from 20.01.2021</p>
-          </div>
+    <div className='dashboard-page'>
+      <div className='dashboard-page-first_column-user_info'>
+        <div className='dashboard-page-first_column-user_info-img'>
+          <img src={avatarUrl} alt='avatar' />
         </div>
-        <div className="dashboard-page-first_column-chart_progress">
-          <canvas id="myChart" width="560" height="310"></canvas>
-          <div>All words progress</div>
+        <div className='dashboard-page-first_column-user_info-data'>
+          <h3>{userName}</h3>
+          <p>Newbie</p>
+          <p>{currentLang.learningFrom} 20.01.2021</p>
         </div>
       </div>
-      <div className="dashboard-page-second_column">
-        <div className="dashboard-page-second_column-info">
-          <div className="dashboard-page-second_column-info-all_words">
-            New words:
-            <p className="dashboard-page-second_column-info-card-stat">3</p>
-          </div>
-          <div className="dashboard-page-second_column-info-best_series">
-            Best streak:
-            <p className="dashboard-page-second_column-info-card-stat">3</p>
-          </div>
-          <div className="dashboard-page-second_column-info-all_cards">
-            Total passed cards:
-            <p className="dashboard-page-second_column-info-card-stat">3</p>
-          </div>
-          <div className="dashboard-page-second_column-info-card_point">
-           
-            Your point:
-            <p className="dashboard-page-second_column-info-card-stat">3</p>
-          </div>
+
+      <div className='dashboard-page-second_column-info'>
+        <div className='dashboard-page-second_column-info-all_words'>
+        {currentLang.newWords}
+          <p className='dashboard-page-second_column-info-card-stat'>3</p>
         </div>
-        <div className="dashboard-page-second_column-chart_diagram">
-          <div className="piechart">
-            <div className="piechart-legend">
-              <div className="piechart-legend-correct"></div>
-              <span>- correct answer</span>
-            </div>
-            <div className="piechart-legend">
-              <div className="piechart-legend-incorrect"> </div>
-              <span>- incorrect answer</span>
-            </div>
-          </div>
-          <canvas id="piechart" width="500" height="275"></canvas>
-          <div>Ratio of correct and incorrect answers for all time</div>
+        <div className='dashboard-page-second_column-info-best_series'>
+        {currentLang.bestStreak}
+          <p className='dashboard-page-second_column-info-card-stat'>3</p>
+        </div>
+        <div className='dashboard-page-second_column-info-all_cards'>
+        {currentLang.totalPassed}
+          <p className='dashboard-page-second_column-info-card-stat'>3</p>
+        </div>
+        <div className='dashboard-page-second_column-info-card_point'>
+        {currentLang.yourPoint}
+
+          <p className='dashboard-page-second_column-info-card-stat'>3</p>
         </div>
       </div>
-      <div className="dashboard-page-third_column">
-        <div className="dashboard-page-third_column-daily">
-          <span>Daily goal:</span>
-          <p>Complete 50 Cards</p>
-          <p>
-            Today you have completed 0 cards. Complete 50 cards to reach the
-            goal.
-          </p>
-          <p>Your best streak today: 0</p>
-        </div>
-        <div className="dashboard-page-third_column-chart_card_progress">
-          <canvas id="CardChart" width="460" height="290"></canvas>
-          <div>Number of cards made by day</div>
+
+      <div className='dashboard-page-third_column-daily'>
+        <span>        {currentLang.dailyGoal}
+</span>
+        <p>{currentLang.complete} {cardsPerDay} {currentLang.cards} </p>
+        <p>
+        {currentLang.todayCoplited}{comonProgress} {currentLang.cards} . {currentLang.complete} {cardsPerDay-comonProgress} {currentLang.reach}
+        </p>
+        <p>{currentLang.bestStreakToday} 0</p>
+      </div>
+
+      <div className='dashboard-page-first-chart_progress'>
+        <div>{currentLang.chartTitle1}</div>
+        <div className='chart-wrapper'> <Line  data={data} options={options} /></div>
+       
+
+      </div>
+      <div className='dashboard-page-second-chart_diagram'>
+        <div>{currentLang.chartTitle2}</div>
+        <canvas id='piechart' height='275'></canvas>
+        <div className='piechart'>
+          <div className='piechart-legend'>
+            <div className='piechart-legend-correct'></div>
+            <span>- {currentLang.correctAnswer}</span>
+          </div>
+          <div className='piechart-legend'>
+            <div className='piechart-legend-incorrect'> </div>
+            <span>- {currentLang.incorrectAnswer}</span>
+          </div>
         </div>
       </div>
+      <div className='dashboard-page-third-chart_card_progress'>
+        <div>{currentLang.chartTitle3}</div>
+        <div className='chart-wrapper'> <Line  data={data} options={options}/></div>
+      </div>
+     
     </div>
   );
 }
