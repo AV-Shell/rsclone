339c26e6a43d048c444c4d357e4eac07bc54021a
Evgeniy <e.ortsukh@gmail.com>
2021-01-31 23:06:06 +0300

    header/feat:add burger-menu style

--- src/components/burger-menu/burger-menu.tsx
+++ src/components/burger-menu/burger-menu.tsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import './burger-menu.scss';
 import { Link } from "react-router-dom";
+import book from './assets/book.svg';
+import fitness from './assets/fitness.svg';
+import gear from './assets/gear.svg';
+import magicButton from './assets/magicButton.svg';
+import speedometer from './assets/speedometer.svg';
 
 function burgerMenu() {
   return (
     <div className="burger-menu" id='burgerMenuID'>
-      {/* <a  className="burger-menu__button" onClick = {handleClick}>
-<span className="burger-menu__lines"></span>
-</a> */}
+    
       <button className="burger-menu-c-hamburger burger-menu-c-hamburger--htx" id='c-hamburgerID' onClick={handleClick}>
         <span>toggle menu</span>
       </button>
       <nav className="burger-menu__nav">
         <ul className="list">
-          <li className="list__item" ><Link to='/dailygoal' className="list__link list__link" onClick={handleClick}>Daily goal</Link>
+          
+          <li className="list__item" ><Link to='/dashboard' className="list__link list__link" onClick={handleClick}>
+            <img src={speedometer} alt=""/>
+            <span>Dashboard</span>  </Link>
           </li>
-          <li className="list__item" ><Link to='/dashboard' className="list__link list__link_active" onClick={handleClick}>Dashboard</Link>
+          <li className="list__item" ><Link to='/training' className="list__link list__link" onClick={handleClick} >
+          <img src={fitness} alt=""/>
+           <span>Training</span> </Link>
           </li>
-          <li className="list__item" ><Link to='/training' className="list__link list__link_active" onClick={handleClick} >Training</Link>
+          <li className="list__item" ><Link to='/vocabulary' className="list__link list__link" onClick={handleClick}><img src={book} alt=""/>
+            <span>Vocabulary</span> </Link>
           </li>
-          <li className="list__item" ><Link to='/vocabulary' className="list__link list__link_active" onClick={handleClick}>Vocabulary</Link>
+          <li className="list__item" ><Link to='/settings' className="list__link list__link" onClick={handleClick}><img src={gear} alt=""/>
+            <span>Settings</span> </Link>
           </li>
-          <li className="list__item" ><Link to='/settings' className="list__link list__link_active" onClick={handleClick}>Settings</Link>
-          </li>
-          <li className="list__item" ><Link to='/magicButton' className="list__link list__link_active" onClick={handleClick}>Magic Button</Link>
+          <li className="list__item" ><Link to='/magicButton' className="list__link list__link" onClick={handleClick}><img src={magicButton} alt=""/>
+            <span>Magic Button</span> </Link>
           </li>
         </ul>
 
