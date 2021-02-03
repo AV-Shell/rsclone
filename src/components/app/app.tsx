13ccd75c4bbab163b5a54394605fb4eff65e5771
Ортюх Евгений Иосифович <e.ortuh@bc.main.by>
2021-02-03 16:52:38 +0300

    register-settings/feat:add style

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -346,7 +346,7 @@ const App: React.FC = () => {
       <Switch>
         <Route
           path="/createsettings" render={() => (
-            <CreateSettings apiService={api} getSettingsCallback={getSettingsCallback} />
+            <CreateSettings apiService={api} getSettingsCallback={getSettingsCallback} isLanguageRU={isLanguageRU} />
           )}
         />
         <Redirect to="/createsettings" />
