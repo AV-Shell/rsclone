62c5757ca521980494dd00d5be0f9560d512a0bc
angoulema <eugenia.labokha@yandex.by>
2021-02-03 19:16:09 +0300

    settings/feat: add markup and styles

--- src/components/settings-page/settings-page.tsx
+++ src/components/settings-page/settings-page.tsx
@@ -161,137 +161,153 @@ const SettingsPage: React.FC<settingsPageProps> = (props: settingsPageProps) => 
     basicSettingsAtention = 'выберите минимум 1 пункт';
   }
 
-  const acceptButton = acceptButtonState ? <button type="submit">Принять</button> : null;
+  const acceptButton = acceptButtonState ? <button type="submit" className="accept-button">Принять</button> : null;
 
   return (
     <div className="settings-page">
-      <h2>Settings page</h2>
-      <h3>Аватарка</h3>
-      <label htmlFor="avatarNumber">
-        {`Выберите Аватарку '${MIN_AVATAR_NUM}-${MAX_AVATAR_NUM}:`}
-        <input
-          onChange={onChangeAvatarNumber}
-          type="number" id="avatarNumber" name="avatarNumber"
-          min={MIN_AVATAR_NUM} max={MAX_AVATAR_NUM} value={avatarNumber}
-        />
-      </label>
-      <div className="image-container">
-        <img src={`${AVA_URL}ava_${avatarNumber}.png`} alt="Avatar" />
+      <div className="subheader">
+        <h2 className="heading2">Settings</h2>
       </div>
-      <h3>Карточка</h3>
-
-      <form onSubmit={onSubmit}>
-        <p>Выберите минимум 1 из 4</p>
-        <div className="attention-message">{basicSettingsAtention}</div>
-        <label htmlFor="cardTranslation">
-          <input
-            type="checkbox" name="cardTranslation" id="cardTranslation"
-            checked={set2.cardTranslation} onChange={onChangeSet2}
-          />
-          показывать перевод
-        </label>
-        <label htmlFor="cardExample">
-          <input
-            type="checkbox" name="cardExample" id="cardExample"
-            checked={set2.cardExample} onChange={onChangeSet2}
-          />
-          показывать пример
-        </label>
-        <label htmlFor="cardExplanation">
-          <input
-            type="checkbox" name="cardExplanation" id="cardExplanation"
-            checked={set2.cardExplanation} onChange={onChangeSet2}
-          />
-          показывать объяснение
-        </label>
-        <label htmlFor="cardWordPronunciation">
-          <input
-            type="checkbox" name="cardWordPronunciation" id="cardWordPronunciation"
-            checked={set2.cardWordPronunciation} onChange={onChangeSet2}
-          />
-          воспроизводить слово
-        </label>
-        <hr />
-        <label htmlFor="cardTranscription">
-          <input
-            type="checkbox" name="cardTranscription" id="cardTranscription"
-            checked={set1.cardTranscription} onChange={onChangeSet1}
-          />
-          транскрипция
-        </label>
-        <label htmlFor="cardImage">
-          <input
-            type="checkbox" name="cardImage" id="cardImage"
-            checked={set1.cardImage} onChange={onChangeSet1}
-          />
-          картинка
-        </label>
-        <label htmlFor="autoSound">
-          <input
-            type="checkbox" name="autoSound" id="autoSound"
-            checked={set1.autoSound} onChange={onChangeSet1}
-          />
-          воспроизводить после ответа
-        </label>
-        <label htmlFor="cardTranslationAfterSuccess">
-          <input
-            type="checkbox" name="cardTranslationAfterSuccess" id="cardTranslationAfterSuccess"
-            checked={set2.cardTranslationAfterSuccess} disabled={set2.cardTranslation}
-            onChange={onChangeSet2}
-          />
-          показывать перевод слова после ответа
-        </label>
-        <label htmlFor="cardExampleTranslation">
-          <input
-            type="checkbox" name="cardExampleTranslation" id="cardExampleTranslation"
-            checked={set2.cardExampleTranslation} disabled={!set2.cardExample} onChange={onChangeSet2}
-          />
-          перевод примера
-        </label>
-        <label htmlFor="cardExplanationTranslation">
-          <input
-            type="checkbox" name="cardExplanationTranslation" id="cardExplanationTranslation"
-            checked={set2.cardExplanationTranslation} disabled={!set2.cardExplanation} onChange={onChangeSet2}
-          />
-          перевод объяснения
-        </label>
-        <label htmlFor="cardExplanationTranslationAfter">
-          <input
-            type="checkbox" name="cardExplanationTranslationAfter" id="cardExplanationTranslationAfter"
-            checked={set2.cardExplanationTranslationAfter} disabled={!set2.cardExplanation} onChange={onChangeSet2}
-          />
-          перевод объяснения после ответа
-        </label>
-        <label htmlFor="cardExampleTranslationAfter">
-          <input
-            type="checkbox" name="cardExampleTranslationAfter" id="cardExampleTranslationAfter"
-            checked={set2.cardExampleTranslationAfter} disabled={!set2.cardExample} onChange={onChangeSet2}
-          />
-          перевод примера после ответа
-        </label>
-        <label htmlFor="answerButton">
-          <input
-            type="checkbox" name="answerButton" id="answerButton"
-            checked={set1.answerButton} onChange={onChangeSet1}
-          />
-          кнопка &quot;Показать ответ&quot;
-        </label>
-        <label htmlFor="statusButtons">
-          <input
-            type="checkbox" name="statusButtons" id="statusButtons"
-            checked={set1.statusButtons} onChange={onChangeSet1}
-          />
-          кнопки статуса слова
-        </label>
-        <label htmlFor="feedbackButtons">
-          <input
-            type="checkbox" name="feedbackButtons" id="feedbackButtons"
-            checked={set1.feedbackButtons} onChange={onChangeSet1}
-          />
-          кнопки интервального повторения
-        </label>
-        {acceptButton}
-      </form>
+      <div className="container">
+        <div className="avatar-settings">
+          <h3>Аватарка</h3>
+          <div className="avatar-settings-field">
+            <label htmlFor="avatarNumber">
+              {`Выберите Аватарку (${MIN_AVATAR_NUM}-${MAX_AVATAR_NUM}): `}
+              <input
+                className="avatar-input"
+                onChange={onChangeAvatarNumber}
+                type="number" id="avatarNumber" name="avatarNumber"
+                min={MIN_AVATAR_NUM} max={MAX_AVATAR_NUM} value={avatarNumber}
+              />
+            </label>
+            <div className="image-container">
+              <img src={`${AVA_URL}ava_${avatarNumber}.png`} alt="Avatar" />
+            </div>
+          </div>
+        </div>
+        <div className="training-settings">
+          <h3>Карточка</h3>
+          <form onSubmit={onSubmit}>
+            <div className="both-settings">
+              <div className="main-settings">
+                <p className="main-settings-info">Выберите минимум 1 из 4</p>
+                <div className="attention-message">{basicSettingsAtention}</div>
+                <label htmlFor="cardTranslation">
+                  <input
+                    type="checkbox" name="cardTranslation" id="cardTranslation"
+                    checked={set2.cardTranslation} onChange={onChangeSet2}
+                  />
+                  показывать перевод
+                </label>
+                <label htmlFor="cardExample">
+                  <input
+                    type="checkbox" name="cardExample" id="cardExample"
+                    checked={set2.cardExample} onChange={onChangeSet2}
+                  />
+                  показывать пример
+                </label>
+                <label htmlFor="cardExplanation">
+                  <input
+                    type="checkbox" name="cardExplanation" id="cardExplanation"
+                    checked={set2.cardExplanation} onChange={onChangeSet2}
+                  />
+                  показывать объяснение
+                </label>
+                <label htmlFor="cardWordPronunciation">
+                  <input
+                    type="checkbox" name="cardWordPronunciation" id="cardWordPronunciation"
+                    checked={set2.cardWordPronunciation} onChange={onChangeSet2}
+                  />
+                  воспроизводить слово
+                </label>
+              </div>
+              <hr />
+              <div className="facultative-settings">
+                <label htmlFor="cardTranscription">
+                  <input
+                    type="checkbox" name="cardTranscription" id="cardTranscription"
+                    checked={set1.cardTranscription} onChange={onChangeSet1}
+                  />
+                  транскрипция
+                </label>
+                <label htmlFor="cardImage">
+                  <input
+                    type="checkbox" name="cardImage" id="cardImage"
+                    checked={set1.cardImage} onChange={onChangeSet1}
+                  />
+                  картинка
+                </label>
+                <label htmlFor="autoSound">
+                  <input
+                    type="checkbox" name="autoSound" id="autoSound"
+                    checked={set1.autoSound} onChange={onChangeSet1}
+                  />
+                  воспроизводить после ответа
+                </label>
+                <label htmlFor="cardTranslationAfterSuccess">
+                  <input
+                    type="checkbox" name="cardTranslationAfterSuccess" id="cardTranslationAfterSuccess"
+                    checked={set2.cardTranslationAfterSuccess} disabled={set2.cardTranslation}
+                    onChange={onChangeSet2}
+                  />
+                  показывать перевод слова после ответа
+                </label>
+                <label htmlFor="cardExampleTranslation">
+                  <input
+                    type="checkbox" name="cardExampleTranslation" id="cardExampleTranslation"
+                    checked={set2.cardExampleTranslation} disabled={!set2.cardExample} onChange={onChangeSet2}
+                  />
+                  перевод примера
+                </label>
+                <label htmlFor="cardExplanationTranslation">
+                  <input
+                    type="checkbox" name="cardExplanationTranslation" id="cardExplanationTranslation"
+                    checked={set2.cardExplanationTranslation} disabled={!set2.cardExplanation} onChange={onChangeSet2}
+                  />
+                  перевод объяснения
+                </label>
+                <label htmlFor="cardExplanationTranslationAfter">
+                  <input
+                    type="checkbox" name="cardExplanationTranslationAfter" id="cardExplanationTranslationAfter"
+                    checked={set2.cardExplanationTranslationAfter} disabled={!set2.cardExplanation} onChange={onChangeSet2}
+                  />
+                  перевод объяснения после ответа
+                </label>
+                <label htmlFor="cardExampleTranslationAfter">
+                  <input
+                    type="checkbox" name="cardExampleTranslationAfter" id="cardExampleTranslationAfter"
+                    checked={set2.cardExampleTranslationAfter} disabled={!set2.cardExample} onChange={onChangeSet2}
+                  />
+                  перевод примера после ответа
+                </label>
+                <label htmlFor="answerButton">
+                  <input
+                    type="checkbox" name="answerButton" id="answerButton"
+                    checked={set1.answerButton} onChange={onChangeSet1}
+                  />
+                  кнопка &quot;Показать ответ&quot;
+                </label>
+                <label htmlFor="statusButtons">
+                  <input
+                    type="checkbox" name="statusButtons" id="statusButtons"
+                    checked={set1.statusButtons} onChange={onChangeSet1}
+                  />
+                  кнопки статуса слова
+                </label>
+                <label htmlFor="feedbackButtons">
+                  <input
+                    type="checkbox" name="feedbackButtons" id="feedbackButtons"
+                    checked={set1.feedbackButtons} onChange={onChangeSet1}
+                  />
+                  кнопки интервального повторения
+                </label>
+              </div>
+            </div>
+            {acceptButton}
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
